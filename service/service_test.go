73fd1fff9ea11bf572cd3baa1bb2277c64f9d79c
sburmistrova <sburmistrovaWork@yandex.ru>
2026-05-08 11:58:54 +0200

    CommitTask2_5

--- service/service_test.go
+++ service/service_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSum(t *testing.T) {
 	result := service.Sum(1, 3)
-	expected := 4
+	expected := 5
 	if result != expected {
 		t.Errorf("Expected: %d, got: %d", expected, result)
 	}
